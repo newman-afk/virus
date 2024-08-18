2f5fcfc3d1e1d8da84661bc49175697ff52ab89b
newman-afk <84593706+newman-afk@users.noreply.github.com>
2024-08-19 05:59:32 +0800

    finished the basis shit, N start warting notes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,72 +2,27 @@
 
 import Link from "next/link";
 
+const volume1Chapters = ["第一章 基础"];
+
+function ChapterLists(chapters: string[]) {
+  return chapters.map((chapter, index) => (
+    <Link
+      href={"notes/volume1/chapter" + (index + 1)}
+      className="max-w-fit"
+      key={chapter}
+    >
+      {chapter}
+    </Link>
+  ));
+}
+
 export default function Notes() {
   return (
     <>
       <div>
         <p className="flex justify-center text-lg">卷1 分子生物学</p>
         <div className="my-7 grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4">
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
-          <Link href="notes/volume1/chapter1" className="max-w-fit">
-            第1章 基础
-          </Link>
+          {ChapterLists(volume1Chapters)}
         </div>
         <p className="flex justify-center text-lg">卷2 致病机理及防治</p>
       </div>
